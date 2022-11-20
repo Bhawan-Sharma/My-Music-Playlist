d5c8d55e3c73b87994b7b1879879be16311b211a
Bhawan sharma <bhawansharma15@gmail.com>
2022-11-20 15:28:39 +0530

    Commit

---
+++ js/music-list.js
@@ -0,0 +1,132 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Takdaa Ravaan",
+    artist: "Motalu Special",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Pyaar to hona hi tha ",
+    artist: "Bhalu Special",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Motalu Love",
+    artist: "Motalu Special",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Meri Zindagi hai tu",
+    artist: "Motalu Special",
+    img: "music-14",
+    src: "music-14"
+  },
+  // {
+  //   name: "Jim - Sun Goes Down",
+  //   artist: "Jim Yosef x Roy",
+  //   img: "music-15",
+  //   src: "music-15"
+  // },
+  {
+    name: "Dil Ibadat ",
+    artist: "Motalu Special",
+    img: "music-16",
+    src: "music-16"
+  },
+
+  {
+    name: "Tere Dar pe Sanam",
+    artist: "Bhalu Special",
+    img: "music-17",
+    src: "music-17"
+  },
+
+  {
+    name: "For Motu - 1",
+    artist: "By Motu ki Berry",
+    img: "music-8",
+    src: "music-8"
+  },
+
+  {
+    name: "For Motu - Jannat",
+    artist: "By Bhalu - Punjabi",
+    img: "music-9",
+    src: "music-9"
+  },
+
+  {
+    name: "For Motu - Shayad",
+    artist: "By Bhalu - Special One",
+    img: "music-10",
+    src: "music-10"
+  },
+
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-1",
+    src: "music-1"
+  },
+
+  {
+    name: "Music-2",
+    artist: "NCS Release",
+    img: "music-2",
+    src: "music-2"
+  },
+
+  {
+    name: "Music-3",
+    artist: "NCS Release",
+    img: "music-3",
+    src: "music-3"
+  },
+
+  {
+    name: "Music-4",
+    artist: "NCS Release",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Music-5",
+    artist: "NCS Release",
+    img: "music-5",
+    src: "music-5"
+  },
+
+  {
+    name: "Music-6",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+
+  {
+    name: "Music-7",
+    artist: "NCS Release",
+    img: "music-7",
+    src: "music-7"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
